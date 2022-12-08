e97be7f84689c2240105d620107b8c98679bc558
GÜNER ARDA IŞIK <l5819058@std.yildiz.edu.tr>
2022-12-08 19:46:49 +0300

    add function names to controller

---
+++ controllers/UserController.js
@@ -0,0 +1,11 @@
+export const signup = (req, res, next) => {
+    // Will be implemented
+}
+
+export const login = (req, res, next) => {
+    // Will be implemented
+}
+
+export const logout = (req, res, next) => {
+    // Will be implemented
+}
